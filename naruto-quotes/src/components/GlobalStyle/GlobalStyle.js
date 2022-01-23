f83543f1329570386d9c7e034af93fa11ac201c2
francienevaz <fran.vfraga@gmail.com>
2022-01-23 14:56:57 -0300

    updated

--- naruto-quotes/src/components/GlobalStyle/GlobalStyle.js
+++ naruto-quotes/src/components/GlobalStyle/GlobalStyle.js
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
-
+import bgImg from '../../img/bg.jpeg';
 
 export const GlobalStyle = createGlobalStyle`
   body {
+    background: url(${bgImg}) center no-repeat;
+    background-size: cover;
     color: #332c36;
     padding: 0;
     margin: 0;
