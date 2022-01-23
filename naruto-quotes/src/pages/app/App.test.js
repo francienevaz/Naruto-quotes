40fe01fc1cc6b9b7d90451626c46bf4ab10d98b5
francienevaz <fran.vfraga@gmail.com>
2022-01-22 23:22:35 -0300

    testing TDD

--- naruto-quotes/src/pages/app/App.test.js
+++ naruto-quotes/src/pages/app/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the app with a button, a quote and button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonEl = screen.getByRole('button');
+  const imageEl = screen.getByRole('img');
+  const textEl = screen.getByRole('p');
+
+  expect(buttonEl).toBeInTheDocument();
+  expect(imageEl).toBeInTheDocument();
+  expect(textEl).toBeInTheDocument();
 });
